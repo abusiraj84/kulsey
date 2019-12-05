2e2a7982f2653fb6201c800df73ba9f9256a6fd0
husam <abusiraj84@gmail.com>
2019-12-05 14:47:38 +0300

    1

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,7 @@
 import 'package:crud/src/ui/formadd/form_add_screen.dart';
 import 'package:crud/src/ui/home/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 GlobalKey<ScaffoldState> _scaffoldState = GlobalKey<ScaffoldState>();
 
@@ -8,10 +9,22 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        //اللون الأساسي
         primaryColor: Colors.orange,
-        accentColor: Colors.orangeAccent,
+        // لون فرعي مثلا اللودينع سيركيولار
+        accentColor: Colors.pink,
       ),
+      localizationsDelegates: [
+        // ... app-specific localization delegate[s] here
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('ar'), // Arabic
+      ],
       home: Scaffold(
         key: _scaffoldState,
         appBar: AppBar(
