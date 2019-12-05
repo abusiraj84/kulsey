13baba758810d7e01baea8f19e582cb88496e2e7
husam <abusiraj84@gmail.com>
2019-12-05 15:51:44 +0300

    111

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,65 @@
-import 'package:crud/src/app.dart';
+import 'package:crud/src/ui/formadd/form_add_screen.dart';
+import 'package:crud/src/ui/home/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
 
 void main() => runApp(App());
+
+GlobalKey<ScaffoldState> _scaffoldState = GlobalKey<ScaffoldState>();
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        //اللون الأساسي
+        primaryColor: Colors.orange,
+        // لون فرعي مثلا اللودينع سيركيولار
+        accentColor: Colors.pink,
+        //الخط الرئيسي للتطبيق
+        fontFamily: 'ExpoArabic-Book'
+      ),
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('ar'), // Arabic
+      ],
+      home: Scaffold(
+        key: _scaffoldState,
+        appBar: AppBar(
+          title: Text(
+            "عالم من الهبل",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          actions: <Widget>[
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  _scaffoldState.currentContext,
+                  MaterialPageRoute(builder: (BuildContext context) {
+                    return FormAddScreen();
+                  }),
+                );
+              },
+              child: Padding(
+                padding: const EdgeInsets.only(right: 16.0),
+                child: Icon(
+                  Icons.add,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+        body: HomeScreen(),
+      ),
+    );
+  }
+}
